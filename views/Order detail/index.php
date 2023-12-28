fa494011bca73cade1b2eb178880fcd509733f23
ThanhThuyPNV <buithithanhthuy08022002@gmail.com>
2023-12-28 15:08:20 +0700

    feater/view order

--- views/Order detail/index.php
+++ views/Order detail/index.php
@@ -4,41 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Your Page Title</title>
-  <style>
-    body {
-      margin: 0;
-      font-family: Arial, sans-serif;
-    }
-
-    header {
-      background-color: #dc3545; /* Đổi màu thành màu đỏ (#dc3545) */
-      color: #fff;
-      padding: 10px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-    }
-
-    header h1 {
-      margin: 0;
-    }
-
-    header .buttons {
-      display: flex;
-    }
-
-    header .buttons button {
-      padding: 8px 16px;
-      border: none;
-      cursor: pointer;
-      color: #fff;
-      background-color: #007bff;
-    }
-
-    header .buttons .list-button {
-      margin-left: 50px; /* Khoảng cách giữa các nút */
-    }
-  </style>
+  <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
@@ -49,20 +15,6 @@
       <button class="list-button" onclick="showList()">List</button>
     </div>
   </header>
-
-  <!-- Nội dung trang web -->
-
-  <script>
-    function printOrder() {
-      // Xử lý khi nhấn nút Print
-      console.log('Printing order...');
-    }
-
-    function showList() {
-      // Xử lý khi nhấn nút List
-      console.log('Showing list...');
-    }
-  </script>
 
 </body>
 </html>
