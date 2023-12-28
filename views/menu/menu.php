c3390cecc233c4129132ca9f5962b3df3cf3c7a9
Thi Le <lehothi22@gmail.com>
2023-12-28 12:00:14 +0700

    add menu

---
+++ views/menu/menu.php
@@ -0,0 +1,101 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title>Menu</title>
+    <link rel = "icon" href ="img/logo.jpg" type = "image/x-icon">
+  </head>
+<body>
+  <!-- Category container starts here -->
+  <div class="container my-3 mb-5">
+    <div class="col-lg-2 text-center bg-light my-3" style="margin:auto;border-top: 2px groove black;border-bottom: 2px groove black;">     
+      <h2 class="text-center">Menu </h2>
+    </div>
+    <div class="row">
+      <!-- Fetch all the categories and use a loop to iterate through categories -->
+      <div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/card-1.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=1">VEG PIZZA</a></h5>
+                      <p class="card-text">A delight for veggie lovers! C... </p>
+                      <a href="viewPizzaList.php?catid=1" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-2.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=2">NON-VEG PIZZA</a></h5>
+                      <p class="card-text">Choose your favourite non-veg ... </p>
+                      <a href="viewPizzaList.php?catid=2" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-3.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=3">PIZZA MANIA</a></h5>
+                      <p class="card-text">Indulge into mouth-watering ta... </p>
+                      <a href="viewPizzaList.php?catid=3" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-4.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=4">SIDES ORDERS</a></h5>
+                      <p class="card-text">Complement your pizza with wid... </p>
+                      <a href="viewPizzaList.php?catid=4" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-5jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=5">BEVERAGES</a></h5>
+                      <p class="card-text">Complement your pizza with wid... </p>
+                      <a href="viewPizzaList.php?catid=5" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/card-6.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=6">CHOICE OF CRUSTS</a></h5>
+                      <p class="card-text">Fresh Pan Pizza Tastiest Pan P... </p>
+                      <a href="viewPizzaList.php?catid=6" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-7.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=7">BURGER PIZZA</a></h5>
+                      <p class="card-text">Domino’s Pizza Introducing a... </p>
+                      <a href="viewPizzaList.php?catid=7" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div><div class="col-xs-3 col-sm-3 col-md-3">
+                  <div class="card" style="width: 18rem;">
+                    <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
+                    <div class="card-body">
+                      <h5 class="card-title"><a href="viewPizzaList.php?catid=8">CHOICE OF TOPPINGS</a></h5>
+                      <p class="card-text">CHOICE OF TOPPINGS... </p>
+                      <a href="viewPizzaList.php?catid=8" class="btn btn-primary">View All</a>
+                    </div>
+                  </div>
+                </div>    
+            </div>
+        </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>         
+</body>
+</html>
