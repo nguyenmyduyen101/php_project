067a002e290a4fad74d410b9cd5ab99bd14e0c18
Thi Le <lehothi22@gmail.com>
2023-12-29 08:02:59 +0700

    add menu

--- views/menu/menu.php
+++ views/menu/menu.php
@@ -1,101 +1,174 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <title>Menu</title>
-    <link rel = "icon" href ="img/logo.jpg" type = "image/x-icon">
-  </head>
+    <title id="title">Category</title>
+    
+</head>
 <body>
-  <!-- Category container starts here -->
-  <div class="container my-3 mb-5">
-    <div class="col-lg-2 text-center bg-light my-3" style="margin:auto;border-top: 2px groove black;border-bottom: 2px groove black;">     
-      <h2 class="text-center">Menu </h2>
-    </div>
-    <div class="row">
-      <!-- Fetch all the categories and use a loop to iterate through categories -->
-      <div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/card-1.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=1">VEG PIZZA</a></h5>
-                      <p class="card-text">A delight for veggie lovers! C... </p>
-                      <a href="viewPizzaList.php?catid=1" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-2.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=2">NON-VEG PIZZA</a></h5>
-                      <p class="card-text">Choose your favourite non-veg ... </p>
-                      <a href="viewPizzaList.php?catid=2" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-3.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=3">PIZZA MANIA</a></h5>
-                      <p class="card-text">Indulge into mouth-watering ta... </p>
-                      <a href="viewPizzaList.php?catid=3" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-4.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=4">SIDES ORDERS</a></h5>
-                      <p class="card-text">Complement your pizza with wid... </p>
-                      <a href="viewPizzaList.php?catid=4" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-5jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=5">BEVERAGES</a></h5>
-                      <p class="card-text">Complement your pizza with wid... </p>
-                      <a href="viewPizzaList.php?catid=5" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/card-6.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=6">CHOICE OF CRUSTS</a></h5>
-                      <p class="card-text">Fresh Pan Pizza Tastiest Pan P... </p>
-                      <a href="viewPizzaList.php?catid=6" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-7.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=7">BURGER PIZZA</a></h5>
-                      <p class="card-text">Domino’s Pizza Introducing a... </p>
-                      <a href="viewPizzaList.php?catid=7" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div><div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewPizzaList.php?catid=8">CHOICE OF TOPPINGS</a></h5>
-                      <p class="card-text">CHOICE OF TOPPINGS... </p>
-                      <a href="viewPizzaList.php?catid=8" class="btn btn-primary">View All</a>
-                    </div>
-                  </div>
-                </div>    
-            </div>
+    <div class="container my-3" id="cont">
+        <div class="col-lg-4 text-center bg-light my-3" style="margin:auto;border-top: 2px groove black;border-bottom: 2px groove black;">     
+            <h2 class="text-center"><span id="catTitle">Items</span></h2>
         </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>         
+        <div class="row">
+        <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/card-1.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">PEPPER BARBECUE CHIC...</h5>
+                                <h6 style="color: #ff0000">Rs. 199/-</h6>
+                                <p class="card-text">Pepper Barbecue Chicken I Che...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=13" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-2.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">CHICKEN SAUSAGE...</h5>
+                                <h6 style="color: #ff0000">Rs. 249/-</h6>
+                                <p class="card-text">Chicken Sausage I Cheese...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=14" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-3.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Chicken Golden Delig...</h5>
+                                <h6 style="color: #ff0000">Rs. 249/-</h6>
+                                <p class="card-text">Mmm! Barbeque chicken with a ...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=15" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-4.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Non Veg Supreme..</h5>
+                                <h6 style="color: #ff0000">Rs. 399/-</h6>
+                                <p class="card-text">Bite into supreme delight of ...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=16" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-5jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Chicken Dominator...</h5>
+                                <h6 style="color: #ff0000">Rs. 319/-</h6>
+                                <p class="card-text">Treat your taste buds with Do...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=17" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/card-6.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">PEPPER BARBECUE & ON...</h5>
+                                <h6 style="color: #ff0000">Rs. 249/-</h6>
+                                <p class="card-text">Pepper Barbecue Chicken I Oni...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=18" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-7.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">CHICKEN FIESTA...</h5>
+                                <h6 style="color: #ff0000">Rs. 199/-</h6>
+                                <p class="card-text">Grilled Chicken Rashers I Per...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=19" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Indi Chicken Tikka...</h5>
+                                <h6 style="color: #ff0000">Rs. 349/-</h6>
+                                <p class="card-text">The wholesome flavour of tand...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=20" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                  </div>      
+
+                  <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Indi Chicken Tikka...</h5>
+                                <h6 style="color: #ff0000">Rs. 349/-</h6>
+                                <p class="card-text">The wholesome flavour of tand...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=20" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                  </div>  
+                  <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Indi Chicken Tikka...</h5>
+                                <h6 style="color: #ff0000">Rs. 349/-</h6>
+                                <p class="card-text">The wholesome flavour of tand...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=20" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                  </div>  
+                  <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Indi Chicken Tikka...</h5>
+                                <h6 style="color: #ff0000">Rs. 349/-</h6>
+                                <p class="card-text">The wholesome flavour of tand...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=20" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                  </div>      
+                  
+                  <div class="col-xs-3 col-sm-3 col-md-3">
+                        <div class="card" style="width: 18rem;">
+                            <img src="../image/cart-8.jpg" class="card-img-top" alt="image for this pizza" width="249px" height="270px">
+                            <div class="card-body">
+                                <h5 class="card-title">Indi Chicken Tikka...</h5>
+                                <h6 style="color: #ff0000">Rs. 349/-</h6>
+                                <p class="card-text">The wholesome flavour of tand...</p>   
+                                <div class="row justify-content-center"><button class="btn btn-primary mx-2" data-toggle="modal" data-target="#loginModal">Add to Cart</button></form>                            
+                                <a href="viewPizza.php?pizzaid=20" class="mx-2"><button class="btn btn-primary">Quick View</button></a> 
+                                </div>
+                            </div>
+                        </div>
+                  </div>  
+          </div>
+    </div>
+</div>
 </body>
 </html>
