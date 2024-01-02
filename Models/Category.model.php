e22a2f60a6cfb13598c36cd99a59d19484bb2027
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-01-02 17:31:24 +0700

    feat/menu_nav

--- Models/Category.model.php
+++ Models/Category.model.php
@@ -1,10 +1,4 @@
 <?php
-try {
-    $connection = new PDO($dsn, $username, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
 
 function get_all_categories()
 {
--- database/database.php
+++ database/database.php
@@ -7,3 +7,10 @@ $password = "";
 
 $dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
 $conn = new PDO($dsn, $username, $password);
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
