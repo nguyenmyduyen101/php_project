e3414f5869c9832f71349c550e8c527ccc65d6c8
Thi Le <lehothi22@gmail.com>
2024-01-02 18:35:20 +0700

    sua duong dan

--- router.php
+++ router.php
@@ -4,7 +4,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     URL_ROOT . URL_SUBFOLDER. '/' => APP_ROOT. '/controllers/home/index.controller.php',
     URL_ROOT . URL_SUBFOLDER. '/Home' => APP_ROOT. '/controllers/home/index.controller.php',
-    URL_ROOT . URL_SUBFOLDER. '/SignUp'=>APP_ROOT. '/controllers/SignUp/SignUp_controller.php',
 
 ];
 
