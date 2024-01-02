37b701345dc2e89a287812d4782930c4326b7eed
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-01-02 17:23:19 +0700

    feat/menu_nav

--- Models/Category.model.php
+++ Models/Category.model.php
@@ -1,16 +1,4 @@
 <?php
-session_start();
-
-
-$hostname = "localhost";
-$database = "test_data_project";
-$username = "root";
-$password = "";
-
-
-$dsn = "mysql:host=$hostname;dbname=$database;charset=utf8mb4";
-
-
 try {
     $connection = new PDO($dsn, $username, $password);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
