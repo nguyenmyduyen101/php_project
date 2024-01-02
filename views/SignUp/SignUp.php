4003a1ec9afa34e09077006af1d5d4e3a0c3c9bf
Thi Le <lehothi22@gmail.com>
2024-01-02 16:23:04 +0700

    edit SignUp

--- views/SignUp/SignUp.php
+++ views/SignUp/SignUp.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng ký</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/mvc/views/css/style.css">
     <link rel="stylesheet" href="./SignUp.css">
 </head>
 <body>
+
 <form action="/php_project/controllers/SignUp/SignUp_controller.php" method="POST" class="form_signup">
-    <div class="row ">
-        <div class="registration-form col-11 ">
+    <div class="row">
+        <div class="registration-form col-11">
             <h1>SignUp</h1>
         </div>
         <div class="cancel_button col-1">
@@ -17,40 +20,26 @@
         </div>
     </div>
 
+    <div>
+        <input type="text" id="username" name="username" placeholder="Username" required><br>
 
-    <div >
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br>
+        <input type="text" id="firstName" name="firstName" placeholder="First Name" required><br>
 
+        <input type="text" id="lastName" name="lastName" placeholder="Last Name" required><br>
 
-        <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required><br>
+        <input type="email" id="email" name="email" placeholder="Email" required><br>
 
+        <input type="tel" id="phone" name="phone" placeholder="Phone No" required><br>
 
-        <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" required><br>
+        <input type="password" id="password" name="password" placeholder="Password" required><br>
 
+        <input type="password" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br>
-
-
-        <label for="phone">Phone No:</label>
-        <input type="tel" id="phone" name="phone" required><br>
-
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br>
-
-
-        <label for="confirmPassword">Confirm Password:</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" required><br>
-
-
-        <input type="submit" value="Submit" >
-</div>
-
+        <input type="submit" value="Submit">
+    </div>
 
     <p>Already have an account? <a href="login.html">Login here</a></p>
+</form>
+
 </body>
 </html>
