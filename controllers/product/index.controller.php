5648fa1c821c0dda28c01d2ea22cfd2f935223cf
nguyenmyduyen101 <136870985+nguyenmyduyen101@users.noreply.github.com>
2024-01-16 15:55:55 +0700

    refactor_showmenu_showImageBd

--- controllers/product/index.controller.php
+++ controllers/product/index.controller.php
@@ -1,7 +1,9 @@
 <?php 
 require_once APP_ROOT ."/Models/Product.model.php";
+require_once APP_ROOT ."/Models/Category.model.php";
 
 $categoryId = $_GET['category_id'];
+$category = get_category($categoryId);
 $products = get_product_by_category_id($categoryId);
 
 require_once APP_ROOT . "/views/menu\menu.php";
